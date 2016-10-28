9c2af5790c18cfb475c4cb58826908129a0b6d16
Peter Buelow <peter.buelow@lifefitness.com>
2016-10-28 17:36:50 -0500

    Adding a little more power saving Upped the random so they don't change so often, but added a new color

--- jackolantern.ino
+++ jackolantern.ino
@@ -9,6 +9,7 @@ MyCandle candle;
 void setup() {
   power_timer1_disable();
   power_adc_disable();
+  power_usi_disable();             // Knocks out TinyWire library
   
   delay(2000);
   FastLED.addLeds<WS2812B, 1>(leds, NUM_LEDS);
@@ -18,12 +19,14 @@ void setup() {
 }
 
 void loop() {
-  uint16_t rval = random16(0, 20000);
+  uint16_t rval = random16(0, 30000);
 
   if (rval == 13)
     candle.init(HUE_PURPLE, HUE_PURPLE - 10, HUE_PURPLE + 5, 55, 50, 5);
   if (rval == 57)
     candle.init(HUE_RED, HUE_RED, HUE_RED + 15, 55, 50, 5);
+  if (rval == 236)
+    candle.init(HUE_GREEN, HUE_GREEN - 10, HUE_GREEN + 10, 55, 50, 5);
 
   candle.runCandle();
   delay(5);
